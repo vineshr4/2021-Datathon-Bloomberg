397047d74dd7420a7f44a6ef091530f79b765b2c
michaelyang1 <myang1394@gmail.com>
2021-10-16 22:17:04 -0500

    Organized clusters into dictionary

--- inference_kmeans.py
+++ inference_kmeans.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from topic_prediction import *
 from sklearn.cluster import KMeans
 from sklearn.metrics import davies_bouldin_score
@@ -18,8 +20,34 @@ def k_means(train_data, k):
     return km.labels_
 
 
+def get_cluster_dict(df, clusters, num_clusters):
+    # create cluster dict
+    cluster_dict = {}
+    for i in range(num_clusters):
+        cluster_dict[i] = []
+
+    # populate cluster dict
+    for i in range(len(clusters)):
+        cluster_val = int(clusters[i])
+        row = df.iloc[i]
+        cluster_dict[cluster_val].append(row)
+
+    return cluster_dict
+
+
 if __name__ == '__main__':
     data = pd.read_csv("fed_doc_data_abs.csv")
-    x_values = np.array(encode_feats(data, 25, "abstract"))
-    clustering = k_means(inference("topic_model_abstract.h5", x_values), 3)
-    print(davies_bouldin_score(x_values, clustering))
+    # x_values = np.array(encode_feats(data, 25, "abstract"))
+    # clustering = k_means(inference("topic_model_abstract.h5", x_values), 3)
+    # # write clustering to a file
+    # out = open('clusters.txt', 'w')
+    # clusters_str = ''
+    # for cluster in clustering:
+    #     clusters_str += str(cluster) + ','
+    # out.write(clusters_str[:-1])
+    # print(davies_bouldin_score(x_values, clustering))
+    # print(clustering)
+    clustering = open('clusters.txt', 'r').readlines()[0].split(',')
+    print(clustering)
+
+    get_cluster_dict(data, clustering, 3)
