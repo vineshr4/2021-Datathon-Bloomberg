c92a872fead70a74858bc4b5a3474f3d4086045d
Vinesh Ravuri <vineshr@tamu.edu>
2021-10-16 23:38:07 -0500

    changed code to analyze clusters, saved x mat for speed

--- analysis.py
+++ analysis.py
@@ -1,36 +1,66 @@
 import pandas as pd
 from inference_kmeans import *
+import matplotlib.pyplot as plt
+import ast
 
-def filter_by_yr(file_name, yr):
+
+def filter_by_year(file_name, year):
     df = pd.read_csv(file_name)
     pub_dates = df['publication_date']
     df_res = pd.DataFrame()
 
     for i in range(len(pub_dates)):
         pub_yr = pub_dates[i][0:4]
-        if pub_yr == yr:
+        if pub_yr == year:
             row = df.iloc[i]
             df_res = df_res.append(row)
 
     return df_res
 
-def make_dataframe():
-    df = pd.read_csv (r'D:/TAMU/Events/Datathon2021/Bloomberg/2021-Datathon-Bloomberg/fed_doc_data_abs.csv')
-    clustering = get_clustering()
-    df['cluster'] = clustering
-    return df
 
-def get_rows_for_cluster(df, cluster):
-    result_df = df.loc[df['cluster'] == cluster]
-    return result_df
+def topics_by_clustering(df, clustering):
+    clustered_topics = [[] for t in range(max(clustering) + 1)]
 
-def get_topics_for_cluster_with_count(df, cluster):
-    df.explode('topics')
-    result_df = get_rows_for_cluster(df, cluster)
-    result = result_df['topics'].value_counts()
-    return result 
+    topics = df["topics"].tolist()
+    topics = [ast.literal_eval(i) for i in topics]
+
+    for i in range(0, len(clustering)):
+        curr_topics = topics[i]
+        clustered_topics[clustering[i]] += curr_topics
+
+    return clustered_topics
+
+
+def topic_frequency(clustered_topics):
+    all_topics = []
+    all_frequency = []
+    num_clusters = len(clustered_topics)
+    for i in range(0, num_clusters):
+        curr_topics = clustered_topics[i]
+        topics = []
+        frequencies = []
+        for j in range(0, len(curr_topics)):
+            if curr_topics[j] not in topics:
+                topics.append(curr_topics[j])
+                frequencies.append(curr_topics.count(curr_topics[j]))
+
+        s = [x for _, x in sorted(zip(frequencies, topics), reverse=True)]
+        frequencies.sort(reverse=True)
+
+        all_topics.append(s)
+        all_frequency.append(frequencies)
+
+    return all_topics, all_frequency
+
 
 if __name__ == '__main__':
-    filter_by_yr('fed_doc_data_abs.csv', '2020')
-    df = make_dataframe()
-    print(get_topics_for_cluster_with_count(df, 0))
+    data = pd.read_csv("fed_doc_data_abs.csv")
+    # x_values = np.array(encode_feats(data, 25, "abstract"))
+    x_values = np.load("x_values.npy")
+    cluster = k_means(inference("topic_model_abstract.h5", x_values), 3)
+    topic_clusters = topics_by_clustering(data, cluster)
+    m_topics, m_frequencies = topic_frequency(topic_clusters)
+
+    for i in range(0, len(m_topics)):
+        print(m_topics[i])
+        print(m_frequencies[i])
--- inference_kmeans.py
+++ inference_kmeans.py
@@ -1,9 +1,6 @@
-import pandas as pd
-
 from topic_prediction import *
 from sklearn.cluster import KMeans
 from sklearn.metrics import davies_bouldin_score
-from analysis import *
 
 
 def inference(model_path, x):
@@ -20,50 +17,12 @@ def k_means(train_data, k):
 
     return km.labels_
 
-def get_clustering():
-    data = pd.read_csv("fed_doc_data_abs.csv")
-    x_values = np.array(encode_feats(data, 25, "abstract"))
-    clustering = k_means(inference("topic_model_abstract.h5", x_values), 3)
-    return clustering
-
-
-def get_cluster_dict(df, clusters, num_clusters):
-    # create cluster dict
-    cluster_dict = {}
-    for i in range(num_clusters):
-        cluster_dict[i] = pd.DataFrame()
-
-    # populate cluster dict
-    for i in range(len(clusters)):
-        cluster_val = int(clusters[i])
-        row = df.iloc[i]
-        cluster_dict[cluster_val] = cluster_dict[cluster_val].append(row)
-
-    return cluster_dict
-
-
-def common_regulations(yr_range):
-    # get all cluster dicts within yr range
-    relevant_cluster_dicts = []
-
-    for yr in range(yr_range[0], yr_range[1] + 1):
-        yr_frames = filter_by_yr('fed_doc_data_abs.csv', str(yr))
-        x_values = np.array(encode_feats(yr_frames, 25, "abstract"))
-        clustering = k_means(inference("topic_model_abstract.h5", x_values), 3)
-
-        cluster_dict = get_cluster_dict(yr_frames, clustering, 3)
-        relevant_cluster_dicts.append(cluster_dict)
-
-    return relevant_cluster_dicts
-
 
 if __name__ == '__main__':
-    # data = pd.read_csv("fed_doc_data_abs.csv")
+    data = pd.read_csv("fed_doc_data_abs.csv")
     # x_values = np.array(encode_feats(data, 25, "abstract"))
     # clustering = k_means(inference("topic_model_abstract.h5", x_values), 3)
     # print(davies_bouldin_score(x_values, clustering))
     # print(clustering)
     #
     # get_cluster_dict(data, clustering, 3)
-    common_regulations((2001, 2006))
-
